51b267c0d4cf8d4421dea686ef2c2ee9abac4405
jsanto26 <55516877+jsanto26@users.noreply.github.com>
2019-11-03 20:34:58 -0700

    Update vw_Total_Loan_Year.sql

--- Views/vw_Total_Loan_Year.sql
+++ Views/vw_Total_Loan_Year.sql
@@ -1,3 +1,23 @@
+/********************************************************************************************
+NAME: [dbo].[vw_Total_Loan_Year]
+PURPOSE: Create the view [dbo].[vw_Total_Loan_Year]
+SUPPORT: Jose Santos
+	  
+MODIFICATION LOG:
+Ver   Date        Author    Description
+----  ----------  -------   -----------------------------------------------------------------
+1.0   11/03/2019  JoseSantos  1. Built this script to create the view [dbo].[vw_Total_Loan_Year].
+RUNTIME: 
+1 min
+NOTES: 
+How much are the loans in years (2019, 2018, 2017, 2016)?
+LICENSE: 
+This code is covered by the GNU General Public License which guarantees end users
+the freedom to run, study, share, and modify the code. This license grants the recipients
+of the code the rights of the Free Software Definition. All derivative work can only be
+distributed under the same license terms.
+********************************************************************************************/
+
 --1) How much are the loans in years (2019, 2018, 2017, 2016)?
 
 CREATE VIEW vw_Total_Loan_Year
