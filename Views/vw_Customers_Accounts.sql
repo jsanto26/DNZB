fb2e9ee529aef93aa530a29dd1e7ebabc7b130bb
jsanto26 <55516877+jsanto26@users.noreply.github.com>
2019-11-04 14:44:34 -0700

    Update vw_Customers_Accounts.sql

--- Views/vw_Customers_Accounts.sql
+++ Views/vw_Customers_Accounts.sql
@@ -1,3 +1,23 @@
+/********************************************************************************************
+NAME: [dbo].[vw_Customers_Accounts]
+PURPOSE: Create the view [dbo].[vw_Customers_Accounts]
+SUPPORT: Jose Santos
+	  
+MODIFICATION LOG:
+Ver   Date        Author    Description
+----  ----------  -------   -----------------------------------------------------------------
+1.0   11/04/2019  JoseSantos  1. Built this script to create the view [dbo].[vw_Customers_Accounts].
+RUNTIME: 
+1 min
+NOTES: 
+How much are the loans in years (2019, 2018, 2017, 2016)?
+LICENSE: 
+This code is covered by the GNU General Public License which guarantees end users
+the freedom to run, study, share, and modify the code. This license grants the recipients
+of the code the rights of the Free Software Definition. All derivative work can only be
+distributed under the same license terms.
+********************************************************************************************/
+--2) Total of Existents Customers and New Customers (2019)
 --2) Total of Existents Customers and New Customers (2019)
 
 CREATE VIEW vw_Customers_Accounts
